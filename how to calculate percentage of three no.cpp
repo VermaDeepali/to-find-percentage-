192e2fe6baa0f82e51be61835cd456c25a7dfc20
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2019-06-23 10:37:01 -0700

    to #calculate #percentage of #three numbers in #c++programming

---
+++ how to calculate percentage of three no.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int p,c,m,per;
+int main()
+{
+	std::cout<<"enter three marks:";
+	std::cin>>p>>c>>m;
+	per=((p+c+m)*100)/300;
+	std::cout<<"percentage is:"<<per;
+	return 0;
+	
+	
+	
+}
